30efe42e74032467740bf7779506c75e3886842b
zhushuai <zhushuai@allydata.cn>
2016-08-09 11:56:42 +0800

    improve auth

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_GET['user']) && isset($_GET['pwd']) && ($_GET['pwd'] == 'allydata') && empty($_COOKIE['user'])) {
     setcookie('user', $_GET['user'], time()+3600*24*365*10);
-    exit('Auth succeed ,pleasz refresh !');
+    header('location: /');
 }
 if (empty($_COOKIE['user'])) {
     exit('Auth Failed');
